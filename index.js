0779ecb85f2d7b20450ca3ed3aab2bd5b7499bd7
mdz786 <mohd17zeeshan17@gmail.com>
2023-01-09 11:56:20 +0530

    add score calculater and declare winner

--- index.js
+++ index.js
@@ -17,44 +17,54 @@ function getPlayerChoice(){
     return prompt("enter either rock/paper/scissor");
 }
 
+let userScore = 0;
+let computerScore = 0;
+
 function play(PlayerChoice,ComputerChoice){
     if(PlayerChoice.length == ComputerChoice.length){
         console.log("The game has been Tied PLAY AGAIN...!");
+        return ++userScore , ++computerScore;
     }
     if(PlayerChoice.length == 4){
         if(ComputerChoice.length == 5){
             console.log(`you choose ${PlayerChoice}
              computer WON n choose ${ComputerChoice}`);
+            return ++computerScore;
         }
     }
     if(PlayerChoice.length == 5){
         if(ComputerChoice.length == 4){
             console.log(`you WON n choose ${PlayerChoice}
             computer choose ${ComputerChoice}`);
+            return ++userScore;
         }
     }
     if(PlayerChoice.length == 4){
         if(ComputerChoice.length == 7){
             console.log(`you WON n choose ${PlayerChoice}
             computer choose ${ComputerChoice}`);
+            return ++userScore;
         }
     }
     if(PlayerChoice.length == 7){
         if(ComputerChoice.length == 4){
             console.log(`you choose ${PlayerChoice}
             computer WON n choose ${ComputerChoice}`);
+            return ++computerScore;
         }
     }
     if(PlayerChoice.length == 5){
         if(ComputerChoice.length == 7){
             console.log(`you choose ${PlayerChoice}
             computer WON n choose ${ComputerChoice}`);
+            return ++computerScore;
         }
     }
     if(PlayerChoice.length == 7){
         if(ComputerChoice.length == 5){
             console.log(`you WON n choose ${PlayerChoice}
             computer choose ${ComputerChoice}`);
+            return ++userScore;
         }
     }
 }
@@ -64,6 +74,18 @@ function game(playCopy){
         playCopy(getPlayerChoice(),getComputerChoice());
         console.log(`You Played ${i} times`);
     }
+    if(userScore > computerScore){
+        console.log(`the user WON the game...!`);
+        console.log(`THE FINAL SCORES ARE ${userScore} ${computerScore}`);
+    }
+    else if(userScore < computerScore){
+        console.log(`the user WON the game...!`);
+        console.log(`THE FINAL SCORES ARE ${userScore} ${computerScore}`);    
+    }
+    else{
+        console.log(`the user WON the game...!`);
+        console.log(`THE FINAL SCORES ARE ${userScore} ${computerScore}`); 
+    }   
 }
 
 game(play);
