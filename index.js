56e2b21540deed2f80558170163ff2cddc982896
mdz786 <mohd17zeeshan17@gmail.com>
2023-01-11 12:57:22 +0530

    to test all buttons

--- index.js
+++ index.js
@@ -12,80 +12,29 @@ function getComputerChoice(){
         return "scissor";
     }
 }
+//dont edit above code...(returns us the ComputerSelection!
 
-function getPlayerChoice(){
-    return prompt("enter either rock/paper/scissor");
+
+//DOM events
+
+const rockButton = document.querySelector('#idrock');
+const display = document.querySelector('.display');
+rockButton.addEventListener('click',testRockButton(getComputerChoice()));
+
+function testRockButton(ComputerSelection){
+    if(ComputerSelection == rockButton.textContent){
+        display.textContent = 'THE ROUND HAS BEEN TIED';
+    }
+    if(ComputerSelection == 'paper'){
+        display.textContent = 'THE COMPUTER WON THIS ROUND';
+    }
+    if(ComputerSelection == 'scissor'){
+        display.textContent = 'THE USER WON THIS ROUND';
+    }
+
 }
 
-let userScore = 0;
-let computerScore = 0;
 
-function play(PlayerChoice,ComputerChoice){
-    if(PlayerChoice.length == ComputerChoice.length){
-        console.log("The game has been Tied PLAY AGAIN...!");
-        return ++userScore , ++computerScore;
-    }
-    if(PlayerChoice.length == 4){
-        if(ComputerChoice.length == 5){
-            console.log(`you choose ${PlayerChoice}
-             computer WON n choose ${ComputerChoice}`);
-            return ++computerScore;
-        }
-    }
-    if(PlayerChoice.length == 5){
-        if(ComputerChoice.length == 4){
-            console.log(`you WON n choose ${PlayerChoice}
-            computer choose ${ComputerChoice}`);
-            return ++userScore;
-        }
-    }
-    if(PlayerChoice.length == 4){
-        if(ComputerChoice.length == 7){
-            console.log(`you WON n choose ${PlayerChoice}
-            computer choose ${ComputerChoice}`);
-            return ++userScore;
-        }
-    }
-    if(PlayerChoice.length == 7){
-        if(ComputerChoice.length == 4){
-            console.log(`you choose ${PlayerChoice}
-            computer WON n choose ${ComputerChoice}`);
-            return ++computerScore;
-        }
-    }
-    if(PlayerChoice.length == 5){
-        if(ComputerChoice.length == 7){
-            console.log(`you choose ${PlayerChoice}
-            computer WON n choose ${ComputerChoice}`);
-            return ++computerScore;
-        }
-    }
-    if(PlayerChoice.length == 7){
-        if(ComputerChoice.length == 5){
-            console.log(`you WON n choose ${PlayerChoice}
-            computer choose ${ComputerChoice}`);
-            return ++userScore;
-        }
-    }
-}
 
-function game(playCopy){
-    for(let i = 1;i<=5;i++){
-        playCopy(getPlayerChoice(),getComputerChoice());
-        console.log(`You Played ${i} times`);
-    }
-    if(userScore > computerScore){
-        console.log(`the user WON the game...!`);
-        console.log(`THE FINAL SCORES ARE ${userScore} ${computerScore}`);
-    }
-    else if(userScore < computerScore){
-        console.log(`the user WON the game...!`);
-        console.log(`THE FINAL SCORES ARE ${userScore} ${computerScore}`);    
-    }
-    else{
-        console.log(`the user WON the game...!`);
-        console.log(`THE FINAL SCORES ARE ${userScore} ${computerScore}`); 
-    }   
-}
 
-game(play);
+
